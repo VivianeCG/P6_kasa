6ef1a30cee00ef9c52965deb5ec1893409ad48ef
VivianeCG <chenet.viviane@gmail.com>
2024-07-31 14:57:25 +0200

    CSS du header et footer, mise en place des routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import '../src/Assets/LogoKasa.PNG'
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Page404 from './Pages/Page404';
+import RentalInfos from './Pages/RentalInfos'
 import Header from './Components/Header';
-import Footer from './Components/Footer';
+import Footer from './Components/Footer.js';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-
-      <Footer />
+      <Header/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/About' element={<About/>}/>
+          <Route path='/Page404' element={<Page404/>}/>
+          <Route path='/RentalInfos' element={<RentalInfos/>}/>
+        </Routes>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import logo from '../Assets/LogoKasa.PNG'
 import '../Styles/Header.css'
+import { Link } from 'react-router-dom';
 function Header() {
     return (<div>
         <header className='header'>
             <img src= {logo} alt="logo Kasa" className='logo'></img>
             <nav className='navbar'>
                 <ul>
-                    <li><a href='#'>Accueil</a></li>
-                    <li><a href='#'>A propos</a></li> 
+                    <li><Link to='/'>Accueil</Link></li>
+                    <li><Link to='/About'>A propos</Link></li> 
                 </ul>
             </nav>
         </header>
