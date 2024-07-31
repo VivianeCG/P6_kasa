8bb9b53dc7fa0125b254b2a157735404aea278c7
VivianeCG <chenet.viviane@gmail.com>
2024-07-31 15:17:19 +0200

    Composant Banner

---
+++ src/Components/Banner.js
@@ -0,0 +1,10 @@
+import '../Styles/Banner.css'
+
+function Banner() {
+    return <div>
+        <div className="banner">
+
+        </div>
+    </div>
+}
+export default Banner;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,9 @@
-
+import Banner from '../Components/Banner';
 
 
 function Home() {
     return <div className='App'>
-
+        <Banner/>
     </div>
 }
 export default Home;
