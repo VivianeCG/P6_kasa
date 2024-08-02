50f1fb136a05a3491fb63633d695c49e3ea79f87
VivianeCG <chenet.viviane@gmail.com>
2024-08-02 16:49:14 +0200

    terminé mise en forme page Error

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -3,13 +3,11 @@ import { Link } from 'react-router-dom';
 
 function Error() {
     return (<div className='error'>
-                <div className='content'>
-                    <p className='number'>404</p>
-                    <p className='sentence'>Oups! La page que vous demandez n'existe pas.</p>
+                    <section className='number'>404</section>
+                    <section className='sentence'>Oups! La page que vous demandez n'existe pas.</section>
                     <nav className='link'>
                         <Link to='/'>Retourner sur la page d'accueil</Link>
                     </nav>
-                </div>
             </div>);
 }
 export default Error;
