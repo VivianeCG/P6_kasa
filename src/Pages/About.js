b3a223f317b706243093778362e2cd44c08f462f
VivianeCG <chenet.viviane@gmail.com>
2024-08-24 11:31:41 +0200

    commit

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,15 +2,19 @@ import Banner from '../Components/Banner.js';
 import photo from '../Assets/photo_montagne.jpg';
 import Collapse from '../Components/Collapse.js';
 import '../Styles/About/About.css';
+import data from '../Assets/aboutInformations.json';
 
 function About() {
     return (<div className='App'>
                 <Banner src={photo} alt="paysage de montagne" />
                 <section className='collapse_wrapper'> 
-                    <Collapse/>
-                    <Collapse/>
-                    <Collapse/>
-                    <Collapse/>
+                    {data.map((info) => (
+                    <Collapse
+                        key={info.id}
+                        title={info.title}
+                        content={info.content}
+                   />
+                    ))}
                 </section>
             </div>);
 }
