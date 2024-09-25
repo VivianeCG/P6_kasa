a1f552f9ca3afcdd54602f88c5becf4efd5ad231
VivianeCG <chenet.viviane@gmail.com>
2024-09-25 11:23:32 +0200

    modifs page 404

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Error() {
     return (<div className='error'>
                     <section className='number'>404</section>
-                    <section className='sentence'>Oups! La page que vous demandez n'existe pas.</section>
+                    <section className='sentence'><div>Oups! La page que </div><div> vous demandez n'existe pas.</div></section>
                     <nav className='link'>
                         <Link to='/'>Retourner sur la page d'accueil</Link>
                     </nav>
